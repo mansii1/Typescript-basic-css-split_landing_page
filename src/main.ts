f9345d6e2f312e28f299675918376cd529f1e905
mansii1 <mansimangukiya0@gmail.com>
2025-08-22 22:42:12 +0530

    Add script

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 import "./style.css";
+
+const left = document.querySelector(".left") as HTMLDivElement;
+const right = document.querySelector(".right") as HTMLDivElement;
+const container = document.querySelector(".container") as HTMLDivElement;
+
+left.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
+});
+left.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-left");
+});
+
+right.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
+});
+right.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
+});
